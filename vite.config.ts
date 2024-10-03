01c5e6e0066ae1920345d84019cf9ec181323081
allen <eepson123@gmail.com>
2024-10-04 00:09:32 +0800

    fix(canvas): fix the path and id

--- vite.config.ts
+++ vite.config.ts
@@ -22,8 +22,8 @@ async function processHtmlFiles(targetDir: string, contentDir: string) {
       let content = await fs.promises.readFile(file, "utf-8");
 
       content = content.replace(
-        /<audio src="\.\.\/public\/([^"]+)" controls type="audio\/ogg"><\/audio>/g,
-        '<audio src="./$1" type="audio/ogg" controls></audio>'
+        /<audio src="\.\.\/public\/([^"]+)" controls type="audio\/ogg" id="audio-player"><\/audio>/g,
+        '<audio src="./$1" type="audio/ogg" controls  id="audio-player"></audio>'
       );
       // Update <source> paths
       content = content.replace(
